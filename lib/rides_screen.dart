c153226641ad820c75df4a7b4ce5ef1206425ea7
Hussain <hussain101608@gmail.com>
2026-06-29 16:58:59 +0530

    rides secion complete office

--- lib/rides_screen.dart
+++ lib/rides_screen.dart
@@ -1,5 +1,5 @@
 import 'dart:ffi';
-
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -84,193 +84,226 @@ class RidesScreen extends StatelessWidget {
           ],
         ),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 12, left: 15, right: 15),
-            child: Container(
-              decoration: BoxDecoration(
-                color: const Color(0xFFE73D4E),
-                borderRadius: BorderRadius.circular(25),
-              ),
-              height: height * 0.13,
-              width: width,
-              child: Padding(
-                padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Where to?",
-                      style: TextStyle(
-                        fontSize: 22,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w800,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 12, left: 15, right: 15),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: const Color(0xFFE73D4E),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                height: height * 0.13,
+                width: width,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Where to?",
+                        style: TextStyle(
+                          fontSize: 22,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w800,
+                        ),
                       ),
+        
+                      SizedBox(height: 10),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: const Color(0xFFFEF5F6),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        height: height * 0.05,
+        
+                        child: Row(
+                          children: [
+                            SizedBox(width: 10),
+        
+                            Icon(
+                              Icons.location_on_outlined,
+                              color: Colors.red.shade500,
+                            ),
+                            SizedBox(width: 10),
+                            Text(
+                              "Enter destination",
+                              style: TextStyle(
+                                color: Colors.grey.shade700,
+                                fontSize: 15,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 15, top: 15, bottom: 12),
+              child: Text(
+                "Choose your ride",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 22,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+            ListView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: vehicleList.length,
+              itemBuilder: (context, index) {
+                final vehicle = vehicleList[index];
+        
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 18,
+                    vertical: 5,
+                  ),
+                  child: Container(
+                    height: height * 0.10,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(color: Colors.grey),
                     ),
-
-                    SizedBox(height: 10),
-                    Container(
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8),
+                          child: Image.asset(vehicle.image, height: 70),
+                        ),
+        
+                        const SizedBox(width: 10),
+        
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              vehicle.title,
+                              style: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+        
+                            Text(
+                              vehicle.subtitle,
+                              style: TextStyle(
+                                fontSize: 17,
+                                color: Colors.grey.shade700,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 15, bottom: 12),
+              child: Text(
+                "Ride Offers",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 22,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+        
+            ListView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: RideOffersList.length,
+              itemBuilder: (context, index) {
+                final offerRide = RideOffersList[index];
+        
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 18,
+                    vertical: 5,
+                  ),
+                  child: DottedBorder(
+                    color: Color(0x80E2132A),
+                    strokeWidth: 2,
+                    dashPattern: const [5, 3], // 10px dash, 5px gap
+                    borderType: BorderType.RRect,
+                    radius: const Radius.circular(15),
+                    child: Container(
+                      height: height * 0.08,
                       decoration: BoxDecoration(
-                        color: const Color(0xFFFEF5F6),
-                        borderRadius: BorderRadius.circular(18),
+                        color: Color(0xFFF9F1F2),
+                        // borderRadius: BorderRadius.circular(20),
+                        // border: Border.all(color: Colors.grey),
                       ),
-                      height: height * 0.05,
-
                       child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          SizedBox(width: 10),
-
-                          Icon(
-                            Icons.location_on_outlined,
-                            color: Colors.red.shade500,
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 18),
+                            child: Column(
+                               mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  offerRide.title,
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                                    
+                                Text(
+                                  offerRide.subtitle,
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.grey.shade700,
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
-                          SizedBox(width: 10),
-                          Text(
-                            "Enter destination",
-                            style: TextStyle(
-                              color: Colors.grey.shade700,
-                              fontSize: 15,
-                              fontWeight: FontWeight.w600,
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 18),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                    border:Border.all(
+                                      color:Color(0xFFE73C4D),
+                                      width: 2,
+                                    ),
+                                    borderRadius: BorderRadius.circular(50),
+                                  ),
+                                  child: Padding(
+                                    padding: EdgeInsets.symmetric(vertical: 3,horizontal: 15),
+                                    child: Text(offerRide.coupen, style: TextStyle(color:Color(0xFFE73C4D),fontSize: 13,fontWeight: FontWeight.w900
+                                    )),
+                                  )),
+                              ],
                             ),
                           ),
                         ],
                       ),
                     ),
-                  ],
-                ),
-              ),
+                  ),
+                );
+              },
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 15, top: 15, bottom: 12),
-            child: Text(
-              "Choose your ride",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 22,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-          ),
-          ListView.builder(
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            itemCount: vehicleList.length,
-            itemBuilder: (context, index) {
-              final vehicle = vehicleList[index];
-
-              return Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 18,
-                  vertical: 5,
-                ),
-                child: Container(
-                  height: height * 0.10,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    border: Border.all(color: Colors.grey),
-                  ),
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 8),
-                        child: Image.asset(vehicle.image, height: 70),
-                      ),
-
-                      const SizedBox(width: 10),
-
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            vehicle.title,
-                            style: const TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-
-                          Text(
-                            vehicle.subtitle,
-                            style: TextStyle(
-                              fontSize: 17,
-                              color: Colors.grey.shade700,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            },
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 15, bottom: 12),
-            child: Text(
-              "Ride Offers",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 22,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-          ),
-
-          ListView.builder(
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            itemCount: RideOffersList.length,
-            itemBuilder: (context, index) {
-              final offerRide = RideOffersList[index];
-
-              return Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 18,
-                  vertical: 5,
-                ),
-                child: Container(
-                  height: height * 0.10,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    border: Border.all(color: Colors.grey),
-                  ),
-                  child: Row(
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            offerRide.title,
-                            style: const TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-
-                          Text(
-                            offerRide.subtitle,
-                            style: TextStyle(
-                              fontSize: 17,
-                              color: Colors.grey.shade700,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        children: [Text(offerRide.coupen, style: TextStyle())],
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            },
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
